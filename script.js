f07583b1480babf0016e5738635a5c8ffb4e490a
Andrew <andrew0352@gmail.com>
2022-03-20 23:59:50 +0200

    Function

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+// let numberOfFilms = prompt('Скільки фільмів ви переглянули?', '');
+// for (let i = 0;
+//     (numberOfFilms == null || numberOfFilms.length == 0); i++) {
+//     numberOfFilms = prompt('Скільки фільмів ви переглянули?', '');
+// }
+
 let numberOfFilms;
-do {
+
+function start() {
     numberOfFilms = prompt('Скільки фільмів ви переглянули?', '');
-    console.log(typeof (numberOfFilms));
-    console.log(numberOfFilms);
+    while (numberOfFilms == null || numberOfFilms.length == 0 || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt('Скільки фільмів ви переглянули?', '');
+    }
 }
-while (numberOfFilms == null || numberOfFilms.length == 0);
+
+start();
+
+
+// let numberOfFilms;
+// do {
+//     numberOfFilms = prompt('Скільки фільмів ви переглянули?', '');
+//     console.log(typeof (numberOfFilms));
+//     console.log(numberOfFilms);
+// }
+// while (numberOfFilms == null || numberOfFilms.length == 0);
 
 
 let personalMoviesDB = {
@@ -25,27 +43,25 @@ if (personalMoviesDB.count <= 10) {
     alert('Error!!!');
 }
 
+// for (let i = 0; i < 2; i++) {
+//     const lastFilm = prompt('Останній переглянутий фільм?', ''),
+//         markFilm = prompt('Яка ваша оцінка фільму?', '');
+
+//     if (lastFilm != null && lastFilm != '' && lastFilm.length < 50 && markFilm != null && markFilm != '' && markFilm.length < 50) {
+//         personalMoviesDB.movies[lastFilm] = markFilm;
+//         console.log('Done!');
+//     } else {
+//         console.log('Error!');
+//         i--;
+//     }
+// }
 for (let i = 0; i < 2; i++) {
-    const lastFilm = prompt('Останній переглянутий фільм?', ''),
+    let lastFilm = prompt('Останній переглянутий фільм?', ''),
         markFilm = prompt('Яка ваша оцінка фільму?', '');
-
-    if (lastFilm != null && lastFilm != '' && lastFilm.length < 50 && markFilm != null && markFilm != '' && markFilm.length < 50) {
-        personalMoviesDB.movies[lastFilm] = markFilm;
-        console.log('Done!');
-    } else {
-        console.log('Error!');
-        i--;
+    while (lastFilm == null || lastFilm == '' || lastFilm.length > 50 || markFilm == null || markFilm == '' || markFilm.length > 50) {
+        lastFilm = prompt('Останній переглянутий фільм?', '');
+        markFilm = prompt('Яка ваша оцінка фільму?', '');
     }
-
-
-
-    // markFilm = prompt('Яка ваша оцінка фільму?', '');
-    // personalMoviesDB.movies[lastFilm] = markFilm;
-
-
-    // markFilm = prompt('Яка ваша оцінка фільму?', '');
-
+    personalMoviesDB.movies[lastFilm] = markFilm;
 }
-
-
 console.log(personalMoviesDB);
